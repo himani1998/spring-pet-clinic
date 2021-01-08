c9beaa666ff9a326d2a86a7da18f0241e773233e
himani1998 <himanibansal1998@gmail.com>
2021-01-07 22:00:06 -0500

    Initial Project Add

---
+++ src/test/java/himani/spring/pet/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package himani.spring.pet.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
