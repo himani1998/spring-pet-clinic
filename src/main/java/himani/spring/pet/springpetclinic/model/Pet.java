7dcaf75ccb9dfcc4e0e829b14bfe187cce868894
himani1998 <himanibansal1998@gmail.com>
2021-01-12 09:31:55 -0500

    Adding Long id.

--- src/main/java/himani/spring/pet/springpetclinic/model/Pet.java
+++ src/main/java/himani/spring/pet/springpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package himani.spring.pet.springpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
     private PetType petType;
     private Owner owner;
 
