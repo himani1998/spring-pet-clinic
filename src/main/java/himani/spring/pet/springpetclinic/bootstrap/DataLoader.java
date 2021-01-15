152cacd2be28ee9f8b5f79eb845e6f88880801ae
himani1998 <himanibansal1998@gmail.com>
2021-01-15 09:53:29 -0500

    Refactored map id implementation to abstract id generation

--- src/main/java/himani/spring/pet/springpetclinic/bootstrap/DataLoader.java
+++ src/main/java/himani/spring/pet/springpetclinic/bootstrap/DataLoader.java
@@ -14,22 +14,20 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService= new OwnerServiceMap();
-        vetService= new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
     public void run(String... args) throws Exception {
         Owner owner1= new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2= new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Cisco");
         owner2.setLastName("White");
 
@@ -37,7 +35,6 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1= new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
@@ -45,7 +42,6 @@ public class DataLoader implements CommandLineRunner {
 
 
         Vet vet2= new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Lex");
         vet2.setLastName("Brown");
 
