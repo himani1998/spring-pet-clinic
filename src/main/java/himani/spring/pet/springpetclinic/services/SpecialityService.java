ffa9e9a019cada86af00886fed9d12287d237423
himani1998 <himanibansal1998@gmail.com>
2021-01-16 10:41:10 -0500

    added speciality map service

---
+++ src/main/java/himani/spring/pet/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package himani.spring.pet.springpetclinic.services;
+
+import himani.spring.pet.springpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality,Long>{
+}
---
+++ src/main/java/himani/spring/pet/springpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package himani.spring.pet.springpetclinic.services.map;
+
+import himani.spring.pet.springpetclinic.model.Speciality;
+import himani.spring.pet.springpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality,Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void Delete(Speciality object) {
+        super.Delete(object);
+    }
+
+    @Override
+    public void DeleteById(Long id) {
+         super.DeleteById(id);
+    }
+}
