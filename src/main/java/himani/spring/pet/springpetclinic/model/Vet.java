401b53a9cebb98fcdffc316517a1c213aca9dd2d
himani1998 <himanibansal1998@gmail.com>
2021-01-16 08:58:42 -0500

    added speciality and added to vet

--- src/main/java/himani/spring/pet/springpetclinic/model/Vet.java
+++ src/main/java/himani/spring/pet/springpetclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package himani.spring.pet.springpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+    private Set<Speciality> speciality;
+
+    public Set<Speciality> getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Set<Speciality> speciality) {
+        this.speciality = speciality;
+    }
 }
