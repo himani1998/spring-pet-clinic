2b052da9ded2c3ebf53d98f8876b7ca25a47d4d4
himani1998 <himanibansal1998@gmail.com>
2021-01-18 10:13:45 -0500

    added spring data jpa for vets, speciality, pets, pet type

--- src/main/java/himani/spring/pet/springpetclinic/services/map/PetMapService.java
+++ src/main/java/himani/spring/pet/springpetclinic/services/map/PetMapService.java
@@ -1,31 +1,32 @@
 package himani.spring.pet.springpetclinic.services.map;
 
-import himani.spring.pet.springpetclinic.model.Vet;
+
+import himani.spring.pet.springpetclinic.model.Pet;
 import himani.spring.pet.springpetclinic.services.CrudService;
-import himani.spring.pet.springpetclinic.services.VetService;
+import himani.spring.pet.springpetclinic.services.PetService;
 import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetService {
+public class PetMapService extends AbstractMapService<Pet,Long> implements PetService {
 
     @Override
-    public Set<Vet> findAll() {
+    public Set<Pet> findAll() {
         return super.findAll();
     }
 
     @Override
-    public Vet findById(Long id) {
+    public Pet findById(Long id) {
         return super.findById(id);
     }
 
     @Override
-    public Vet save(Vet object) {
+    public Pet save(Pet object) {
         return super.save(object);
     }
 
     @Override
-    public void Delete(Vet object) {
+    public void Delete(Pet object) {
         super.Delete(object);
     }
 
--- src/main/java/himani/spring/pet/springpetclinic/services/map/VetMapService.java
+++ src/main/java/himani/spring/pet/springpetclinic/services/map/VetMapService.java
@@ -1,32 +1,31 @@
 package himani.spring.pet.springpetclinic.services.map;
 
-
-import himani.spring.pet.springpetclinic.model.Pet;
+import himani.spring.pet.springpetclinic.model.Vet;
 import himani.spring.pet.springpetclinic.services.CrudService;
-import himani.spring.pet.springpetclinic.services.PetService;
+import himani.spring.pet.springpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet,Long> implements PetService {
+public class VetMapService extends AbstractMapService<Vet,Long> implements VetService {
 
     @Override
-    public Set<Pet> findAll() {
+    public Set<Vet> findAll() {
         return super.findAll();
     }
 
     @Override
-    public Pet findById(Long id) {
+    public Vet findById(Long id) {
         return super.findById(id);
     }
 
     @Override
-    public Pet save(Pet object) {
+    public Vet save(Vet object) {
         return super.save(object);
     }
 
     @Override
-    public void Delete(Pet object) {
+    public void Delete(Vet object) {
         super.Delete(object);
     }
 
