3e229cb8c2fc056b19206a9483e07183e6c06de6
himani1998 <himanibansal1998@gmail.com>
2021-01-17 20:06:28 -0500

    update vets and visits with JPA mapping entity

--- src/main/java/himani/spring/pet/springpetclinic/model/Vet.java
+++ src/main/java/himani/spring/pet/springpetclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package himani.spring.pet.springpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name="vets")
 public class Vet extends Person{
+
+    @ManyToMany
+    @JoinTable(name="vet_specialities",joinColumns = @JoinColumn(name="vet_id"),
+            inverseJoinColumns = @JoinColumn(name="speciality_id"))
     private Set<Speciality> speciality= new HashSet<>();
 
     public Set<Speciality> getSpeciality() {
